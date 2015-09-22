fc8074f822a18cd61474565d33194ef1997973b7
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-22 17:59:57 +0300

    Hack Carrierwave to get file path

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,10 @@
 class Feed < ActiveRecord::Base
   belongs_to :user
   mount_uploaders :file, FileUploader
+
+  def storage_path
+    directory = File.dirname self.file[0].path
+    basename = File.basename(self.file[0].path).split("\"")[1]
+    File.join directory, basename
+  end
 end

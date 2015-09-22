a2f83bd415b4ef50a87038ddf7b30fee6c61f4aa
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-23 00:39:45 +0300

    Add CSV parsing method

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,6 +2,12 @@ class Feed < ActiveRecord::Base
   belongs_to :user
   mount_uploaders :file, FileUploader
 
+  def parse_products
+    CSV.foreach(storage_path, headers: true) do |row|
+      puts row.to_hash
+    end
+  end
+
   def storage_path
     directory = File.dirname self.file[0].path
     basename = File.basename(self.file[0].path).split("\"")[1]

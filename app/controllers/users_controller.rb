c633ec9f7ab2239085024ab82685eec4b307d487
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-22 13:30:32 +0300

    Add authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :authorize_resource!, only: [:show]
 
   def show
   end
+
+  private
+
+    def authorize_resource!
+      if !params[:id]
+        redirect_to user_path current_user
+        flash[:alert] = "Hey! Hackers are not allowed on our site ;)"
+      elsif current_user.id != params[:id].to_i
+        redirect_to user_path current_user
+        flash[:alert] = "Hey! Stealing other people's property is illegal. Data is private property!"
+      end
+    end
 end

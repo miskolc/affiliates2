7af59093bf125d9ca6ce84cd7637d210edaf8ea1
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-24 22:56:52 +0300

    Hack pagination links on the client

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,17 @@
+
+// $(document).ready(function () {
+//   $('.pagination .previous_page').text('«');
+//   $('.pagination .next_page').text('»');
+
+//   $('.pagination').each(function () {
+//     var $bar = $(this);
+//     $bar.find('a, .current, span').wrapAll('<ul>');
+//     $(".pagination ul").addClass("pagination");
+//     $bar.find('a, .current, span').wrap('<li>');
+
+//     $bar.find('em').each(function () {
+//       $(this).parent().addClass('disabled');
+//       $(this).replaceWith('<a href="#">' + $(this).text() + '</a>');
+//     });
+//   });
+// });

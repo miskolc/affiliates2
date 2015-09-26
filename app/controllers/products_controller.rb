979159e8b162ba6ca7532e1b80310f75aac5a129
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-26 11:48:10 +0300

    DRY up the controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,23 @@
 class ProductsController < ApplicationController
   before_action :get_feed
+  before_action :get_product, except: [:index, :new, :create]
 
   def index
     @products = @feed.products_on_page params[:page]
   end
 
   def show
-    @product = @feed.products.find(params[:id])
   end
 
   def edit
-    @product = @feed.products.find(params[:id])
   end
 
   def update
-    @product = @feed.products.find(params[:id])
     @product.update_attributes product_params
     redirect_to edit_user_feed_product_path(current_user, @feed, @product, page: params[:page] )
   end
 
   def destroy
-    @product = @feed.products.find(params[:id])
     @product.destroy
     redirect_to user_feed_products_path(current_user, @feed, page: params[:page])
   end
@@ -28,6 +25,10 @@ class ProductsController < ApplicationController
   private
     def get_feed
       @feed = current_user.feeds.find params[:feed_id]
+    end
+
+    def get_product
+      @product = @feed.products.find(params[:id])
     end
 
     def product_params
